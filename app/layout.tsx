2fd6fe304912dffdbade9ec0a07dc57fd4bab2de
c_patel <chanakya1patel@gmail.com>
2023-09-25 04:38:30 -0400

    tinybird script for per site analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,11 @@ import { Analytics } from "@vercel/analytics/react";
 import { Providers } from "./providers";
 import { Metadata } from "next";
 import { cn } from "@/lib/utils";
+import Script from 'next/script'
+
 
 const title =
-  "Platforms Starter Kit – The all-in-one starter kit for building multi-tenant applications.";
+  "Dubspace – Infinite Sites. Infinite Possibilities.";
 const description =
   "The Platforms Starter Kit is a full-stack Next.js app with multi-tenancy and custom domain support. Built with Next.js App Router, Vercel Postgres and the Vercel Domains API.";
 const image = "https://vercel.pub/thumbnail.png";
@@ -28,6 +30,7 @@ export const metadata: Metadata = {
     creator: "@vercel",
   },
   metadataBase: new URL("https://vercel.pub"),
+
 };
 
 export default function RootLayout({
@@ -37,6 +40,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          defer
+          src="https://unpkg.com/@tinybirdco/flock.js"
+          data-host="https://api.us-east.tinybird.co"
+          data-token="p.eyJ1IjogIjFmZDJjZTQ5LTQyYmYtNGI4Zi1hYmJlLTQzZjM4ZWVhNTQ1ZCIsICJpZCI6ICI4N2JjZGMwZC04Yjk1LTQxYzUtOTVkYy0zZTg3MWFkOGY2OTMiLCAiaG9zdCI6ICJ1c19lYXN0In0.jnFCqCv7UObwIQ4KQ9_SJSoP_3jfCnjs6DU_lj6L4SQ"
+        ></Script>
+      </head>
+
+
       <body className={cn(cal.variable, inter.variable)}>
         <Providers>
           {children}
